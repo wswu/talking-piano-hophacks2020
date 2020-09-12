11f29177d408686c5d9fe713e934cdb33d936c9e
Cicero Lu <ciceroxlu@gmail.com>
2020-09-12 14:23:14 -0400

    Add a midi2score conversion bash script

---
+++ src/midi2score.bash
@@ -0,0 +1,7 @@
+mscore -M ../data/midi_import_options_0.xml -I ../data/yuzo.mid -o ../data/yuzo.pdf
+
+mscore -M ../data/midi_import_options_0.xml -I ../data/yuzo.mid -o ../data/yuzo.
+mp3
+
+mscore -M ../data/midi_import_options_0.xml -I ../data/yuzo.mid -o ../data/yuzo.
+mscz
