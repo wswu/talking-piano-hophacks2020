defb3d9046c3947843875929cae74218a7243e6e
winston <wswu24@gmail.com>
2020-09-12 10:46:30 -0400

    use stft, compute frequency with db

--- src/process_audio.py
+++ src/process_audio.py
@@ -38,18 +38,31 @@ def make_chord(freqs):
     return c
 
 
+def mute_low_volume(seq):
+    return [x if x > -40 else -100 for x in seq]
+
 def compute_top_frequencies(spec, n_peaks):
+    bin2freq = dict(enumerate(librosa.fft_frequencies(sr=48000, n_fft=4096)))
     top_freqs = []
     for time_slice in spec.T:
         pitches = []
-        time_slice = time_slice[:32]  # remove high frequencies (32 mel = 3218 Hz)
-        time_slice = savgol_filter(time_slice, 7, 3)  # smooth the curve
+        intensities = []
+        time_slice = time_slice[:172]  # remove high frequencies (2048: bin 128 = 3000 Hz, bin 86 = 2015 Hz)
+        # 4096: 256 = 3000 Hz, 172 = 2015 Hz
+        
+        # time_slice = mute_low_volume(time_slice) # causes div by zero?!
 
+        # filter out frequencies < 70 Hz
+        for i in range(6):
+            time_slice[i] = -100
+
+        time_slice = savgol_filter(time_slice, 9, 3)  # smooth the curve
         for (idx, value) in peaks(time_slice)[:n_peaks]:
-            hz = librosa.mel_to_hz(idx)
+            hz = bin2freq[idx]
             pitches.append(hz)
+            intensities.append(value)
         pitches.sort()
-        top_freqs.append(pitches)
+        top_freqs.append((pitches, intensities))
     return top_freqs
 
 
@@ -62,28 +75,42 @@ def write(path, piece):
 
 
 def generate_midi(data, sample_rate):
-    spec = librosa.feature.melspectrogram(y=data.T[0], sr=sample_rate, n_fft=20000)
+    # spec = librosa.feature.melspectrogram(y=data.T[0], sr=sample_rate, n_fft=20000)
 
-    top_freqs = compute_top_frequencies(spec, n_peaks=8)
+    spec = librosa.stft(data.T[0], n_fft=4096, hop_length=512)
+
+    db = librosa.amplitude_to_db(spec, ref=np.max)
+
+    top_freqs = compute_top_frequencies(db, n_peaks=5)
     
     piece = []
-    for freqs in top_freqs:
+    for freqs, intensities in top_freqs:
         piece.append(make_chord(freqs))
         
-    write("yuzo.mid", piece)
+    write("winston.mid", piece)
 
 
 def plot_spec(spec):
     fig, ax = plt.subplots()
-    S_dB = librosa.power_to_db(spec, ref=np.max)
-    img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sample_rate, fmax=8000, ax=ax)
+    # S_dB = librosa.power_to_db(spec, ref=np.max)
+    # img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sample_rate, fmax=8000, ax=ax)
+    img = librosa.display.specshow(librosa.amplitude_to_db(spec, ref=np.max),y_axis='log', x_axis='time', ax=ax)
     fig.colorbar(img, ax=ax, format='%+2.0f dB')
-    ax.set(title='Mel-frequency spectrogram')
+    ax.set(title='stft')
     plt.savefig("plot.png")
 
+def plot_db(timeslice):
+    bin2freq = dict(enumerate(librosa.fft_frequencies(sr=48000, n_fft=2048)))
+    plt.figure()
+    plt.plot([bin2freq[b] for b in range(0, 128)], timeslice[:128])
+    plt.xlabel('freq Hz')
+    plt.ylabel('dB')
+    plt.xscale('log')
+    plt.show()
+    
 
 def main():
-    data, sample_rate = sf.read("data/yuzo.wav", dtype='float32')
+    data, sample_rate = sf.read("data/winston.wav", dtype='float32')
     generate_midi(data, sample_rate)
 
 
