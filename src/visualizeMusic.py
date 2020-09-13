eb14e4307363a568d537c14dd55da603b20b4486
Yuzo Ishikawa <65511234+yuyuishi12@users.noreply.github.com>
2020-09-12 20:16:52 -0400

    Update visualizeMusic.py

--- src/visualizeMusic.py
+++ src/visualizeMusic.py
@@ -6,17 +6,15 @@ Created on Fri Sep 11 23:52:32 2020
 """
 
 import os
-os.chdir(r"C:\Users\yuzoi\OneDrive\Desktop\JHU\hophacks2020\quick-midi-animation-master")
 import main as mp
 import configparser
 
 
 configfile = 'options.cfg'
 midifile = 'yuzo.mid'
-
 config = configparser.ConfigParser()
 config.read(configfile)
-config.set("DEFAULT", "midi_filename", './data/' + midifile )
+config.set("DEFAULT", "midi_filename", '../data/' + midifile )
 with open(configfile, "w") as f:
     config.write(f)
 
