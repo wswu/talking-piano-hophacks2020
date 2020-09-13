3ef3f0c3a4a1774d45f99691ab118ff8b9156b67
winston <wswu24@gmail.com>
2020-09-13 02:22:19 -0400

    feedback form submissin, back button, caching output

--- src/visualize.py
+++ src/visualize.py
@@ -17,13 +17,17 @@ def visualize(input_midi, output_mp4):
     config.read(configfile)
     config.set("DEFAULT", "midi_filename", input_midi)
     config.set("DEFAULT", "mp4_filename", output_mp4)
+
+    if os.path.exists(output_mp4):
+        os.remove(output_mp4)
+
     with open(configfile, "w") as f:
         config.write(f)
 
     mp.main(CONFIG=configfile)
     input_video = ffmpeg.input(output_mp4)
     input_audio = ffmpeg.input(input_midi)
-    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(output_mp4).run()
+    # ffmpeg.concat(input_video, input_audio, v=1, a=1).output(output_mp4).run()
 
 def main():
     visualize("output.mid", "output.mp4")
--- src/webapp.py
+++ src/webapp.py
@@ -18,25 +18,43 @@ class WebApp(object):
 
     @cherrypy.expose
     def upload(self, fileToUpload, n_voices, keydiff_threshold, submit):
-        dir_prefix = 'src/website/html5up-dimension/products/'
+        name = fileToUpload.filename
+        print('name ', name)
+        dir_prefix = 'src/website/html5up-dimension/products/' + name
 
         with open("input.wav", "wb") as fout:
             fout.write(fileToUpload.file.read())
 
         print('processing audio')        
-        process_audio.wav2midi("input.wav", dir_prefix+"output.mid",  {'n_peaks': int(n_voices), 'keydiff_threshold': int(keydiff_threshold)})
+        if not os.path.exists(dir_prefix + "-output.mid"):
+            process_audio.wav2midi("input.wav", dir_prefix + "-output.mid",  {'n_peaks': int(n_voices), 'keydiff_threshold': int(keydiff_threshold)})
 
         print('generating pdf')
-        musescore_call.generate_pdf(dir_prefix+"output.mid", dir_prefix+"output.pdf")
+        if not os.path.exists(dir_prefix + "-output.pdf"):
+            musescore_call.generate_pdf(dir_prefix + "-output.mid", dir_prefix + "-output.pdf")
         
         print('generating mp3')
-        musescore_call.generate_mp3(dir_prefix+"output.mid", dir_prefix+"output.mp3")
+        if not os.path.exists(dir_prefix + "-output.mp3"):
+            musescore_call.generate_mp3(dir_prefix + "-output.mid", dir_prefix + "-output.mp3")
 
         print('generating video')
-        visualize.visualize(dir_prefix+"output.mid", dir_prefix+"output.mp4")
+        if not os.path.exists(dir_prefix + "-output.mp4"):
+            visualize.visualize(dir_prefix + "-output.mid", dir_prefix + "-output.mp4")
 
         with open("src/website/html5up-dimension/pianotalks.html") as f:
-            return f.read()
+            return f.read().replace("OUTPUT_FILENAME", name)
+
+    @cherrypy.expose
+    def survey(self, *args, **kwargs):
+        # return "hi"
+        print(args)
+        print(kwargs)
+        raise cherrypy.HTTPRedirect('/')
+    
+
+    @cherrypy.expose
+    def goback(self):
+        raise cherrypy.HTTPRedirect('/')
 
 
 if __name__ == '__main__':
